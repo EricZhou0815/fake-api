41b9fa968d13ffb5c9f1ade34502f07d9d1d500c
ericzhou <eric.zhou0815@gmail.com>
2025-03-03 11:56:28 +1300

    setup deploy config

--- index.js
+++ index.js
@@ -157,6 +157,9 @@ app.get('/search-shoes', async (req, res) => {
 
 // Start the server
 const port = process.env.PORT || 3000;
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
